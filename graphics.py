dbc76ac261f6ba007006657bf849012bb6128cfe
barry <barryhamzata@gmail.com>
2025-05-09 11:03:13 +0200

    new draw lines

--- graphics.py
+++ graphics.py
@@ -34,9 +34,11 @@ class Point:
         self.y = y
 
 class Line:
-    def __init__(self, x1, y1, x2, y2):
-        self.start = Point(x1, y1)
-        self.end = Point(x2, y2)
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
-        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(100, 100), Point(200, 200))
+    win.draw_line(l, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
